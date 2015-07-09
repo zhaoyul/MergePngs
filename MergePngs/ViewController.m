f53e5957bb3a38590811fb92fa82467f723f0b2a
Kevin.Li <li_zhaoyu@msn.com>
2015-07-09 15:19:44 +0800

    fix

--- MergePngs/ViewController.m
+++ MergePngs/ViewController.m
@@ -23,14 +23,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.view addSubview:self.imageView];
     self.imageView.frame = CGRectMake(0, 0, IMG_WIDTH, IMG_WIDTH);
     self.imageView.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
  
 }
 
+- (UIImage*) createImageWithImage:(UIImage*) image1 andImage:(UIImage*) image2
+
+{
+    NSAssert( CGSizeEqualToSize(image1.size, image2.size), @"image 1 and image 2 should have equal size");
+    
+    CGSize offScreenSize = image1.size;
+    
+    UIGraphicsBeginImageContext(offScreenSize);
+    [image1 drawInRect:CGRectMake(0, 0, offScreenSize.width, offScreenSize.height)];
+    [image2 drawInRect:CGRectMake(0, 0, offScreenSize.width, offScreenSize.height)];
+    UIImage* imagez = UIGraphicsGetImageFromCurrentImageContext();
+    if (image1 && image2)
+    {
+        
+    }
+    
+    UIGraphicsEndImageContext();
+    return imagez;
+
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    static NSInteger counter = 0;
+    NSString *imageName = @(++counter).stringValue;
+    if (counter >= 6) {
+        return;
+    }
+    UIImage *image = [self createImageWithImage:self.imageView.image andImage:[UIImage imageNamed:imageName]];
+    self.imageView.image = image;
 }
 
 @end
